15eb25dbf552c2cf8f8400e933d64c39401c29c2
soniparashar340 <76548183+soniparashar340@users.noreply.github.com>
2022-06-11 22:35:39 +0530

    Create Mystats.py

---
+++ Mystats.py
@@ -0,0 +1,24 @@
+              
+  
+  
+  
+  
+  
+  
+  
+  
+  
+               from life import lessons, experience
+               from Tg import User 
+    
+    #          // life motto
+                     if (sad()===true){
+                        sad().stop();
+                        beAwesome();
+                           },
+               @User 
+                   check(), 
+                       status.
+                 User </Unavailable> 
+                 Username: "@C_O_O_L_dude"
+                     
